80eb92c49150552af154f43d756fe44710f71c04
IlyaDoronin <ryletd@gmail.com>
2021-08-25 22:46:49 +0300

    deploy

--- src/store/endpoints.js
+++ src/store/endpoints.js
@@ -1,5 +1,5 @@
-// export const HOST = "http://api.ryletd.ru";
-export const HOST = "http://localhost:5500";
+export const HOST = "http://api.ryletd.ru";
+// export const HOST = "http://localhost:5500";
 
 export const ENDPOINTS = {
     FOLLOW_ENDPOINT: "/follow",
