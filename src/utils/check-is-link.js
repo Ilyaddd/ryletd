e6d29fea99a5ef808b61d54a58ae3188b07a245e
IlyaDoronin <ryletd@gmail.com>
2021-11-06 19:15:12 +0300

    Удалена провека ссылки

--- src/utils/check-is-link.js
+++ src/utils/check-is-link.js
@@ -1,4 +1,7 @@
 export const checkIsLink = (link) => {
+    // Отмена проверки
+    return link;
+
     var pattern = new RegExp(
         "^(https?:\\/\\/)?" + // protocol
             "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
